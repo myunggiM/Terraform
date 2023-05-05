05c2621e76cd575d7c3ec6ea2d72e88b6bfbe368
myunggi_moon <myunggi_moon@naver.com>
2023-05-05 21:01:06 +0900

    vpc subnet create

---
+++ chapter8/vpc.tf
@@ -0,0 +1,27 @@
+resource "aws_vpc" "main"{
+  cidr_block = "10.0.0.0/16"
+
+  tags ={
+    Name ="terraform-101"
+  }
+}
+
+resource "aws_subnet" "public_subnet"{
+  vpc_id = aws_vpc.main.vpc_id
+  cidr_block ="10.0.0.0/24"
+  
+  availability_zone ="ap-northeast-2a"
+
+  tags ={
+    Name= "terraform-101-public_subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet"{
+  vpc_id = aws_vpc.main.id
+  cidr_block ="10.0.0.0/24"
+
+  tags ={
+    Name = "terraform-101-private_subnet"
+  }
+}
